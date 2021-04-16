f303426b2f2dec37b975cc6a2774bf506841df7d
Zaid J. Barghouthi <zaid.b@eefis.com>
2021-04-17 02:50:46 +0300

    enable smooth corners style

--- src/boot/paints.js
+++ src/boot/paints.js
@@ -1,8 +1,4 @@
 import smoothCornersWorkletUrl from 'file-loader!smooth-corners'
 export default () => {
-  const alreadyRegistered = sessionStorage.getItem('alreadyRegistered')
-  if (alreadyRegistered === null) {
-    CSS.paintWorklet.addModule(smoothCornersWorkletUrl)
-    sessionStorage.setItem('alreadyRegistered', 'yes')
-  }
+  CSS.paintWorklet.addModule(smoothCornersWorkletUrl)
 }
