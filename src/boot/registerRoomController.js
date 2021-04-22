1e91413a854f7f5a20c27b2f197ad6580c1d085c
Amr Barghouthi <30410268+AmrBarghouthi@users.noreply.github.com>
2021-04-23 00:35:49 +0300

    refactor: use didicated class to make all clubhouse API calls

--- src/boot/registerRoomController.js
+++
@@ -1,9 +0,0 @@
-import RoomController from 'src/RoomController'
-
-export default ({ Vue, store }) =>
-{
-  const userIdResolver = () => store.getters['auth/userId']
-  const chAuthTokenResolver = () => store.getters['auth/authToken']
-  const roomController = new RoomController(userIdResolver,chAuthTokenResolver)
-  Vue.prototype.$roomController = roomController
-}
