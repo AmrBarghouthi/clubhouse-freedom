b8e507cea875090efa9b9406289dd6e79f4bd6b2
Amr Barghouthi <barghouthi.a@gmail.com>
2021-04-22 23:54:57 +0300

    registering ClubhouseApi as vue global

---
+++ src/boot/registerVueGlobals.js
@@ -0,0 +1,17 @@
+import RoomController from 'src/RoomController'
+import ClubhouseApi from 'src/ClubhouseApi'
+
+export default ({ Vue, store }) =>
+{
+  const userIdResolver = () => store.getters['auth/userId']
+  const chAuthTokenResolver = () => store.getters['auth/authToken']
+  const clubhouseApi = new ClubhouseApi(()=> {
+    return {
+      userId: userIdResolver(),
+      authToken: chAuthTokenResolver()
+    }
+  })
+  const roomController = new RoomController(userIdResolver,clubhouseApi)
+  Vue.prototype.$clubhouseApi = clubhouseApi
+  Vue.prototype.$roomController = roomController
+}
