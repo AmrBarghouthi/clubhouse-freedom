6e7b08ad58454d652cfed9187144010c6f3db740
Zaid J. Barghouthi <barghouthi.z@gmail.com>
2021-05-05 01:10:20 +0300

    build: update electron version 5.0 -> 11.0
    @babel/plugin-proposal-nullish-coalescing-operator and
    @babel/plugin-proposal-optional-chaining are used because
    webpack 4 does not support these features. If upgraded to
    webpack 5 these plugins should be removed.

--- babel.config.js
+++ babel.config.js
@@ -3,4 +3,8 @@ module.exports = {
   presets: [
     '@quasar/babel-preset-app',
   ],
+  plugins: [
+    '@babel/plugin-proposal-nullish-coalescing-operator',
+    '@babel/plugin-proposal-optional-chaining',
+  ],
 }
